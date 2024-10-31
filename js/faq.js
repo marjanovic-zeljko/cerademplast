e639c20a186d006affae96fd3d20b24f99db330c
marjanovic-zeljko <101601784+marjanovic-zeljko@users.noreply.github.com>
2024-10-31 09:46:00 -0700

    Update faq.js

--- js/faq.js
+++ js/faq.js
@@ -1,10 +1,10 @@
-const $ = document;
-          let img_btn = $.querySelectorAll("img");
-          let items = $.querySelectorAll(".item");
+// const $ = document;
+//           let img_btn = $.querySelectorAll("img");
+//           let items = $.querySelectorAll(".item");
           
-          img_btn.forEach((item) => {
-            item.addEventListener("click", (e) => {
-              item.classList.toggle("act_img");
-              e.target.parentElement.parentElement.classList.toggle("active");
-            });
-          });
+//           img_btn.forEach((item) => {
+//             item.addEventListener("click", (e) => {
+//               item.classList.toggle("act_img");
+//               e.target.parentElement.parentElement.classList.toggle("active");
+//             });
+//           });
