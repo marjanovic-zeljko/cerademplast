ad38eb5605f501bb181aabb7ccb84fba0b319f6a
Max <marjanoviczeljko92@gmail.com>
2024-10-31 17:17:18 +0100

    Added changes

---
+++ js/faq.js
@@ -0,0 +1,13 @@
+// const $ = document;
+// let img_btn = $.querySelectorAll("img");
+// let items = $.querySelectorAll(".item");
+
+// img_btn.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     item.classList.toggle("act_img");
+//     e.target.parentElement.parentElement.classList.toggle("active");
+//   });
+// });
+
+console.log(img_btn);
+console.log(items);
